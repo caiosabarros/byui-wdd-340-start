9ebccedf9459fd5502bf15fabc746486a4ba07d0
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2024-11-04 18:04:18 -0300

    initial deploy of simples app

--- server.js
+++ server.js
@@ -9,11 +9,26 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine","ejs")
+app.use(expressLayouts)
+app.set("layout","./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// Index route
+app.get("/", function(req,res) {
+// The "res" is the response object, while "render()" is an Express function that will retrieve the specified view - "index" - to be sent back to the browser.
+	res.render("index", {title:"Home"})
+// The curly braces are an object (treated like a variable), which holds a name - value pair. This object supplies the value that the "head" partial file expects to receive. The object is passed to the view.
+})
 
 /* ***********************
  * Local Server Information
