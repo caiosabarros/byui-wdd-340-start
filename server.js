5369d7348db5b64b2a3d2d9595c25d35cc8880e2
caiohenriquesa1@gmail.com <caiohenriquesa1@gmail.com>
2024-11-18 18:46:25 -0300

    build the classification view

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ const app = express()
 const static = require("./routes/static")
 const expressLayouts = require("express-ejs-layouts")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine","ejs")
+app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout","./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
